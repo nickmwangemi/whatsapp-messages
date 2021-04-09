fe3fe772fa40011c0220003d3192c145759801bd
nickmwangemi <nickmwangemi@gmail.com>
2021-04-09 20:30:36 +0300

    Create webhook connecting to Twilio

--- whatsapp/views.py
+++ whatsapp/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.http import HttpResponse, response
+from django.views.decorators.csrf import csrf_exempt
+from twilio.twiml.messaging_response import MessagingResponse
 
-# Create your views here.
+
+@csrf_exempt
+def message(request):
+    user = request.POST.get('From')
+    message = request.POST.get('Body')
+    print('\n *** New Message ***')
+    print(f'\n{user} says {message}')
+    print('\n *******************\n')
+
+    response = MessagingResponse()
+    response.message(
+        'Thank you for your message! A member of our team will be in touch with you soon.'
+        )
+
+    return HttpResponse(str(response))
